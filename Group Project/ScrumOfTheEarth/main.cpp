7134986f3ce8b54f68d8e618a2e834b2404ccc22
yosmo78 <yosmo78@gmail.com>
2018-11-11 15:00:52 -0800

    added username check

--- Group Project/ScrumOfTheEarth/main.cpp
+++ Group Project/ScrumOfTheEarth/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     Window w;
+    w.setFixedSize(400, 300);
     w.show();
 
     return a.exec();
--- Group Project/ScrumOfTheEarth/window.cpp
+++ Group Project/ScrumOfTheEarth/window.cpp
@@ -1,6 +1,6 @@
 #include "window.h"
 #include "ui_window.h"
-
+#include <QMessageBox>
 Window::Window(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Window)
@@ -15,7 +15,12 @@ Window::~Window()
 
 void Window::on_pushButton_clicked()
 {
-
+    QString username = ui->UserText->text();
+    QString password = ui->PassText->text();
+    if(username != "admin" || password != "admin")
+    {
+        QMessageBox::warning(this,"Login","You have entered an incorrect password!");
+    }
 }
 
 void Window::on_PassText_returnPressed()
