05fdd0aa7d3a7d94052d83109c887044c2200f15
Brendan Sakamoto <brendansakamoto@Brendans-MacBook-Air.local>
2018-11-13 11:08:53 -0800

    added image to contact us

--- Group Project/ScrumOfTheEarth/contactus.cpp
+++ Group Project/ScrumOfTheEarth/contactus.cpp
@@ -5,11 +5,6 @@ ContactUs::ContactUs(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ContactUs)
 {
-    char relPath[] = "/SCRUM LOGO.jpg";
-    size_t relPathSize = strlen(relPath);
-    char * absPath = getcwd(relPath,100*relPathSize);
-    QPixmap pic(absPath);
-    ui->label->setPixmap(pic);
     ui->setupUi(this);
 
 }
