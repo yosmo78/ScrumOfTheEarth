9a812c723145414f56cae0d4a2bf88c2be0dc63f
yosmo78 <yosmo78@gmail.com>
2018-11-13 11:14:40 -0800

    added some stuff

--- Group Project/ScrumOfTheEarth/contactus.cpp
+++ Group Project/ScrumOfTheEarth/contactus.cpp
@@ -2,18 +2,17 @@
 #include "ui_contactus.h"
 #include <limits.h>
 #include <stdlib.h>
+#include<string.h>
 ContactUs::ContactUs(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ContactUs)
 {
-  // const char path[100] = "SCRUM LOGO.png";
-  // char buf[PATH_MAX+1];
-  // char * ptr = realpath(path,buf);
-  //  QPixmap pic(buf);
+//   const char path[100] = "SCRUM LOGO.png";
+//   char buf[PATH_MAX+1];
+//   char * ptr = realpath(path,buf);
+//    QPixmap pic(buf);
     ui->setupUi(this);
-  //  ui->label->setPixmap(pic);
-  //  ui->label->setMask(pic.mask());
-
+//    ui->label->setPixmap(pic);
 }
 
 ContactUs::~ContactUs()
