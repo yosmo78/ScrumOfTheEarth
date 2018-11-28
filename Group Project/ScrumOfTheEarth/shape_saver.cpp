72967f3149691d08dea52efed69068f4f109c681
yosmo78 <yosmo78@gmail.com>
2018-11-27 22:04:24 -0800

    updated with some type fun

--- Group Project/ScrumOfTheEarth/shape_saver.cpp
+++ Group Project/ScrumOfTheEarth/shape_saver.cpp
@@ -6,28 +6,77 @@
 #include "polygon.h"
 #include "polyline.h"
 #include "text.h"
+#include <string>
 #include <fstream>
-
+#include <typeinfo>
+#include <map>
 
 void shape_saver(myStd::vector<Shape*>& vec, const char* filename)
 {
-    std::ofstream fin;
+    std::map<const std::type_info*, std::string> typeMap;
+    typeMap[&typeid(Square)] = "Square";
+    typeMap[&typeid(Rectangle)] = "Rectangle";
+    typeMap[&typeid(Line)] = "Line";
+    typeMap[&typeid(Circle)] = "Circle";
+    typeMap[&typeid(Ellipse)] = "Ellipse";
+    typeMap[&typeid(Polygon)] = "Polygon";
+    typeMap[&typeid(Polyline)] = "Polyline";
+    typeMap[&typeid(Text)] = "Text";
+
+
+    std::ofstream fout;
     if(vec.size() == 0)
     {
-        fin.open("Error.txt",std::ios_base::out|std::ios_base::app);
-        fin << "Tried to save an empty vector\n";
-        fin.close();
+        fout.open("Error.txt",std::ios_base::out|std::ios_base::app);
+        fout << "Tried to save an empty vector\n";
+        fout.close();
         return;
     }
-    fin.open(filename);
-    if(!fin.fail())
+    fout.open(filename);
+    if(!fout.fail())
     {
         myStd::vector<Shape*>::const_iterator tmp = vec.begin();
         for(int i = 0;i < vec.size() ;++i)
         {
-
-            tmp[i]->getShapeId();
+            fout << '\n';
+            fout << "ShapeId: "<<tmp[i]->getShapeId() << '\n';
+            fout << "ShapeType: " << typeMap[&typeid(**tmp)]<< '\n';
+            fout << "ShapeDimensions: ";
+            if(&typeid(**tmp) == &typeid(Square))
+            {
+              Square * sptr = dynamic_cast<Square *>(*tmp);
+              fout << sptr->getCornerPoint().x() << ", " << sptr->getCornerPoint().y()
+                   << ", "<< sptr->getLength() << '\n';
+            }
+            else if(&typeid(**tmp) == &typeid(Rectangle))
+            {
+              Rectangle * rptr = dynamic_cast<Rectangle *> (*tmp);
+            }
+            else if(&typeid(**tmp) == &typeid(Line))
+            {
+              Line * lptr = dynamic_cast<Line *> (*tmp);
+            }
+            else if(&typeid(**tmp) == &typeid(Circle))
+            {
+              Circle * cptr = dynamic_cast<Circle *> (*tmp);
+            }
+            else if(&typeid(**tmp) == &typeid(Ellipse))
+            {
+              Ellipse * eptr = dynamic_cast<Ellipse *> (*tmp);
+            }
+            else if(&typeid(**tmp) == &typeid(Polygon))
+            {
+              Polygon * pptr = dynamic_cast<Polygon *> (*tmp);
+            }
+            else if(&typeid(**tmp) == &typeid(Polyline))
+            {
+              Polyline * pptr = dynamic_cast<Polyline *> (*tmp);
+            }
+            else if(&typeid(**tmp) == &typeid(Text))
+            {
+              Text * tptr = dynamic_cast<Text *> (*tmp);
+            }
         }
     }
-    fin.close();
+    fout.close();
 }
