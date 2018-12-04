26265a8c207e6300c42cdbfcd67c3363be631b64
yosmo78 <yosmo78@gmail.com>
2018-12-04 10:53:54 -0800

    added get functions

--- Group Project/ScrumOfTheEarth/getstrings.h
+++ Group Project/ScrumOfTheEarth/getstrings.h
@@ -4,4 +4,12 @@
 std::string getPenStyle(int ps);
 std::string getColor(int color);
 
+std::string getPenCapStyle(int pcs);
+std::string getPenJoinStyle(int pjs);
+std::string getBrushStyle(int bs);
+
+std::string getTextAlignment(int ta);
+std::string getFontStyle(int fs);
+std::string getFontWeight(int fw);
+
 #endif // GETSTRINGS_H
