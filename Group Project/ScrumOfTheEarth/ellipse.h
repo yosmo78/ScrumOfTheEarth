4d9e8ba23dc7ab0572f14b57fb1e24841723fa3c
WakkaTrout <ajverburg@hotmail.com>
2018-12-04 19:53:25 -0800

    Added some comments

--- Group Project/ScrumOfTheEarth/ellipse.h
+++ Group Project/ScrumOfTheEarth/ellipse.h
@@ -14,19 +14,74 @@ class Ellipse : public Shape
         void setTopLeft(QPoint p){point = p;}
         void setMajorAxis(int j){semimajor = j;}
         void setMinorAxis(int n){semiminor = n;}
+        /*!
+            Returns the color of the pen
+        */
         Qt::GlobalColor getPenColor() {return penColor;}
+
+        /*!
+            Returns the color of the brush
+        */
         Qt::GlobalColor getBrushColor(){return brushColor;}
+
+        /*!
+            Sets the pen color to the given color
+        */
         void setPenColor(Qt::GlobalColor pc){penColor = pc;}
+
+        /*!
+            Sets the color of the brush to the given brush
+        */
         void setBrushColor(Qt::GlobalColor bc){brushColor = bc;}
+
+        /*!
+            Returns the style of the pen
+        */
         Qt::PenStyle getPenStyle(){return penStyle;}
+
+        /*!
+            Sets the pen style to the given style
+        */
         void setPenStyle(Qt::PenStyle ps){penStyle = ps;}
+
+        /*!
+             Returns the cap style of the pen
+        */
         Qt::PenCapStyle getPenCapStyle() {return penCapStyle;}
+
+        /*!
+            Sets the pen cap style to the given pen cap style
+        */
         void setPenCapStyle(Qt::PenCapStyle pcs){ penCapStyle = pcs;}
+
+        /*!
+            Returns the join style of the pen
+        */
         Qt::PenJoinStyle getPenJoinStyle(){return penJoinStyle;}
+
+        /*!
+            Sets the pen join style to the given pen join style
+        */
         void setPenJoinStyle(Qt::PenJoinStyle pjs){penJoinStyle = pjs;}
+
+        /*!
+            Returns the brush style
+        */
         Qt::BrushStyle getBrushStyle(){return brushStyle;}
+
+        /*!
+            Sets the brush style to the given style
+        */
         void setBrushStyle(Qt::BrushStyle bs){brushStyle = bs;}
+
+        /*!
+            Returns the width of the pen
+        */
         int getPenWidth(){return penWidth;}
+
+        /*!
+            Sets the pen width to the given width
+        */
         void setPenWidth(int pw){penWidth = pw;}
         virtual void draw(QPainter & paint,bool);
         virtual int getType(){return 5;}
