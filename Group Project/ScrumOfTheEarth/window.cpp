d51f3964aff34d5346a532d9541bf53c6f339b90
WakkaTrout <ajverburg@hotmail.com>
2018-12-06 00:23:00 -0800

    Attempted to free more memory, but to no avail(Probably Qt's fault)

--- Group Project/ScrumOfTheEarth/window.cpp
+++ Group Project/ScrumOfTheEarth/window.cpp
@@ -11,6 +11,17 @@ Window::Window(QWidget *parent) :
     setAttribute(Qt::WA_DeleteOnClose);
     ui->GuestLogin->setAttribute(Qt::WA_DeleteOnClose);
     ui->label->setAttribute(Qt::WA_DeleteOnClose);
+    ui->PassText->setAttribute(Qt::WA_DeleteOnClose);
+    ui->Password->setAttribute(Qt::WA_DeleteOnClose);
+    ui->UserText->setAttribute(Qt::WA_DeleteOnClose);
+    ui->Username->setAttribute(Qt::WA_DeleteOnClose);
+    ui->centralWidget->setAttribute(Qt::WA_DeleteOnClose);
+    ui->groupBox->setAttribute(Qt::WA_DeleteOnClose);
+    ui->layoutWidget->setAttribute(Qt::WA_DeleteOnClose);
+    ui->pushButton->setAttribute(Qt::WA_DeleteOnClose);
+    ui->layoutWidget1->setAttribute(Qt::WA_DeleteOnClose);
+    ui->menuBar->setAttribute(Qt::WA_DeleteOnClose);
+    ui->Contact->setAttribute(Qt::WA_DeleteOnClose);
 }
 
 Window::~Window()
